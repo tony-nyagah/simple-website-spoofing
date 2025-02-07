246fb52e662aba9f898f49470cc0a4520e6c6d70
Antony Nyagah <tony.m.nyagah@gmail.com>
2025-02-07 18:53:33 +0300

    update

--- server/index.js
+++ server/index.js
@@ -67,6 +67,6 @@ app.post('/api/submit-credentials', async (req, res) => {
 });
 
 const PORT = process.env.NODE_PORT || 3001;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on port ${PORT} (0.0.0.0)`);
 });
